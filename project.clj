06d502a0fe9f117c6bd2f1ac1acdda2f6fe7370e
prabhanshuguptagit <prabhanshu98@gmail.com>
2019-07-24 14:40:18 +0530

    dependency, ring setup

--- project.clj
+++ project.clj
@@ -3,5 +3,22 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+
+                 [org.postgresql/postgresql "42.2.5"]
+                 [org.clojure/java.jdbc "0.7.9"]
+                 [honeysql "0.9.4"]
+                 [toucan "1.12.0"]
+                 [ragtime "0.8.0"]
+
+                 [ring/ring-core "1.7.1"]
+                 [ring/ring-jetty-adapter "1.7.1"]
+                 [ring/ring-devel "1.7.1"]
+                 [bidi "2.1.6"]
+                 [ring/ring-json "0.4.0"]
+
+                 [environ "1.1.0"]]
+
+  :ring {:handler villagebook.core/app-handler}
+  :plugins [[lein-ring "0.12.5"]]
   :repl-options {:init-ns villagebook.core})
--- src/villagebook/core.clj
+++ src/villagebook/core.clj
@@ -1,6 +1,31 @@
-(ns villagebook.core)
+(ns villagebook.core
+  (:require [ring.adapter.jetty :as jetty]
+            [bidi.ring :refer [make-handler]]
+            [bidi.bidi :as bidi]
+            [ring.util.response :as res]
+            [ring.middleware.params :refer [wrap-params]]
+            [clojure.java.jdbc :as jdbc]
+            [environ.core :refer [env]]))
 
 (defn foo
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
+
+;; Setup routes
+(def routes
+  ["/" {}])
+
+;; Setup handler with the routes
+(def handler
+  (make-handler routes))
+
+;; Setup middleware on the handler
+(def app-handler
+  (-> handler
+      wrap-params))
+
+;; For lein run (heroku)
+(defn -main [& [port]]
+  (let [port (Integer. (or port (env :port) 5000))]
+    (jetty/run-jetty app-handler {:port port :join? false})))
