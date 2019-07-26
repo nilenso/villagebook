47caa8cc3803e2f4133069a9cefb8ceb10606d4c
prabhanshuguptagit <prabhanshu98@gmail.com>
2019-07-26 17:15:28 +0530

    Login tests

--- test/villagebook/auth.clj
+++ test/villagebook/auth.clj
@@ -23,3 +23,31 @@
     (let [request {}
           response (auth-handlers/login request)]
       (is (= 400 (:status response)))))
+
+
+(deftest login-tests
+  (testing "Logging in as user 1"
+    (let [request  {:params user1}
+          response (auth-handlers/login request)]
+      (is (= 200 (:status response)))
+      (is (not (nil? (get-in response [:body "token"]))))))
+
+  (testing "Logging in with invalid request"
+    (let [request {}
+          response (auth-handlers/login request)]
+      (is (= 400 (:status response)))))
+
+  (testing "Logging in as user 1 invalid request - missing password"
+    (let [request {:params (dissoc user1 :password)}
+          response (auth-handlers/login request)]
+       (is (= 400 (:status response)))))
+
+  (testing "Logging in as user 1 with incorrect password"
+    (let [request {:params (assoc user1 :password "newpassword")}
+          response (auth-handlers/login request)]
+      (is (= 401 (:status response)))))
+
+  (testing "Logging in with invalid email (does not exist)"
+    (let [request {:params (assoc user1 :email "random@example.org")}
+          response (auth-handlers/login request)]
+      (is (= 401 (:status response))))))
