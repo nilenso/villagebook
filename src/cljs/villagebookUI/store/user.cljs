e9bbd98037cbdeae07254e046df9a1cfa6d28ee5
prabhanshuguptagit <prabhanshu98@gmail.com>
2019-09-16 18:11:19 +0530

    Keywordize store keys when adding

--- src/cljs/villagebookUI/store/user.cljs
+++ src/cljs/villagebookUI/store/user.cljs
@@ -1,12 +1,13 @@
 (ns villagebookUI.store.user
   (:require [reagent.core :as r]
+            [clojure.walk :refer [keywordize-keys]]
             [villagebookUI.store.state :refer [state]]))
 
 (def user
   (r/cursor state [:user]))
 
 (defn add! [userdata]
-  (swap! user assoc :data userdata))
+  (swap! user assoc :data (keywordize-keys userdata)))
 
 (defn read []
   (:data @user))
