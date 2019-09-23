8827bbc9baae2412ccabb88021df0b7c6302b79d
prabhanshuguptagit <prabhanshu98@gmail.com>
2019-09-23 17:54:55 +0530

    Extract on-click and refactor form inputs(login and signup)

--- src/cljs/villagebookUI/components/login.cljs
+++ src/cljs/villagebookUI/components/login.cljs
@@ -1,52 +1,42 @@
 (ns villagebookUI.components.login
   (:require [reagent.core :as r]
-            [accountant.core :as accountant]
+            [villagebookUI.helpers :as helpers :refer [validate-input]]
+            [villagebookUI.components.utils :refer [reqd-input]]
+            [villagebookUI.api.auth :as auth-api]))
 
-            [villagebookUI.api.auth :as auth]
-            [villagebookUI.api.user :as user-api]
-            [villagebookUI.store.user :as user-store]))
-
-(defn validate!
-  [elementID formdata]
-  (if (.checkValidity (.getElementById js/document elementID))
-    (swap! formdata assoc-in [:validation-classes elementID] "")
-    (swap! formdata assoc-in [:validation-classes elementID] "check-invalid")))
-
-(defn input [formdata k placeholder type & required]
-  [:input.form-control.mt-3.mt-3 {:placeholder placeholder
-                                  :name        k
-                                  :id          k
-                                  :on-change   #(swap! formdata assoc-in [:user k] (-> % .-target .-value))
-                                  :on-blur     #(validate! (name k) formdata)
-                                  :type        type
-                                  :required    required
-                                  :class       (str
-                                                (get-in @formdata [:validation-classes (name k)]) " "
-                                                (get-in @formdata [:validation-classes "login-form"]))}])
+(defn submit-login
+  [user show-validness show-error]
+  (helpers/submit-auth-form :login-form auth-api/login user show-validness show-error))
 
 (defn login []
-  (let [formdata (r/atom {})
-        error    (r/atom {})]
+  (let [user      (r/atom {})
+        validness (r/atom {})
+        error     (r/atom {})
+        fields    [{:id :email :type :email :placeholder "Email"}
+                   {:id :password :type :password :placeholder "Password"}]]
     (fn []
       [:div.l-page-center.formbox
        [:a.brand {:href "/"} "villagebook"]
        [:form#login-form.mt-5.form-group
-        [input formdata :email "Email" "email" :required]
-        [input formdata :password "Password" "password" :required]
+        (doall
+         (for [field fields
+               :let  [id (:id field)]]
+           [reqd-input {:id          id
+                        :key         id
+                        :type        (:type field)
+                        :placeholder (:placeholder field)
+                        :class       [:form-control :mt-3 (get @validness id)(:form @validness)]
+                        :on-change   #(swap! user assoc id %)
+                        :on-blur     #(validate-input id
+                                                      (fn [v] (swap! validness assoc id v)))}]))
         [:div.auth-error (:message @error)]
         [:button.btn.btn-outline-primary.login-btn.mt-2
-         {:type    "submit"
-          :on-click #(do
-                       (.preventDefault %)
-                       (validate! "login-form" formdata)
-                       (auth/login
-                        (:user @formdata)
-                        (fn [res]
-                          (swap! error assoc :message "")
-                          (user-api/get-data!)
-                          (accountant/navigate! "/dashboard"))
-                        (fn [res]
-                          (swap! error assoc :message (:response res)))))}
+         {:type     "submit"
+          :on-click (fn [e]
+                      (.preventDefault e)
+                      (submit-login @user
+                                    #(swap! validness assoc :form %)
+                                    #(swap! error assoc :message %)))}
          "Login"]]
        [:span.small "Don't have an account? "]
        [:a {:href "/signup"} "Signup"]])))
--- src/cljs/villagebookUI/helpers.cljs
+++ src/cljs/villagebookUI/helpers.cljs
@@ -1,4 +1,6 @@
-(ns villagebookUI.helpers)
+(ns villagebookUI.helpers
+  (:require [villagebookUI.api.user :as user-api]
+            [accountant.core :as accountant]))
 
 (defn random-color []
   (str "hsl(" (->> (.random js/Math)
@@ -9,3 +11,29 @@
   [el handler]
   (if (= (.-key el) "Escape")
     (handler)))
+
+(defn html5-valid?
+  [element-id]
+  (->> element-id
+      name
+      (.getElementById js/document)
+      .checkValidity))
+
+(defn validate-input
+  [element-id show-validness]
+  (if (html5-valid? element-id)
+    (show-validness :valid)
+    (show-validness :invalid)))
+
+(defn submit-auth-form
+  [form-id api-fn form-data show-validness show-error]
+  (validate-input form-id show-validness)
+  (if (html5-valid? form-id)
+    (api-fn
+     form-data
+     (fn [res]
+       (show-error "")
+       (user-api/get-data!)
+       (accountant/navigate! "/dashboard"))
+     (fn [res]
+       (show-error (:response res))))))
