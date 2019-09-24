0f5f1e3045f47f8eb39b96dead4cb075aa19c2bf
prabhanshuguptagit <prabhanshu98@gmail.com>
2019-09-24 18:21:49 +0530

    Add delete org button to dashboard

--- src/cljs/villagebookUI/components/main_content.cljs
+++ src/cljs/villagebookUI/components/main_content.cljs
@@ -1,7 +1,38 @@
 (ns villagebookUI.components.main-content
-  (:require [villagebookUI.store.organisations :as org-store]))
+  (:require [villagebookUI.store.organisations :as org-store]
+            [villagebookUI.api.organisation :as org-api]
+            [villagebookUI.fetchers :as fetchers]))
+
+(declare navbar content-box delete-org-btn delete-org)
 
 (defn main-content []
-  [:div.main-content
-   [:div.navbar
-    [:h5 (:name (org-store/get-selected))]]])
+  (let [org (org-store/get-selected)]
+    [:div.main-content
+     [navbar org]
+     (if org
+       [content-box]
+       [:h5 "Oops, page not found"])]))
+
+(defn- navbar
+  [org]
+  [:div.navbar
+   [:h5 (:name org)]
+   [delete-org-btn org]])
+
+(defn content-box []
+  [:div])
+
+(defn delete-org-btn
+  [org]
+  (if (= (:permission org) "owner")
+    [:a {:href     "#"
+         :style    {:height "30px"}
+         :on-click #(delete-org org)}
+     "Delete organisation"]))
+
+(defn delete-org
+  [org]
+  (if (js/confirm (str "Are you sure you want to delete " (:name org) "?"))
+    (org-api/delete (:id org)
+                    #(fetchers/fetch-orgs! first)
+                    identity)))
