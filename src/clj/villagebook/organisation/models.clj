b571384dc8248041bbc0a5b2ad2acd2f147b9c09
prabhanshuguptagit <prabhanshu98@gmail.com>
2019-09-25 16:45:54 +0530

    Add is-owner? helper

--- src/clj/villagebook/organisation/models.clj
+++ src/clj/villagebook/organisation/models.clj
@@ -23,11 +23,15 @@
   [user-id]
   {:success (db/retrieve-by-user user-id)})
 
+(defn is-owner?
+  [org-id user-id]
+  (= (db/get-permission user-id org-id) (name OWNER_PERMISSION)))
+
 (defn delete!
   [user-id id]
   "Check if user is owner of the organisation and delete it"
   (jdbc/with-db-transaction [trn (config/db-spec)]
-    (if (= (db/get-permission user-id id) (name OWNER_PERMISSION))
+    (if (is-owner? id user-id)
       (if-not (empty? (db/delete! id))
         {:success "Organisation deleted"})
       {:error "Invalid permission"})))
