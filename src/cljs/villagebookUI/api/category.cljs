b2bdf44b554f00dd77ae23fc174fce9e6cf1f1f6
prabhanshuguptagit <prabhanshu98@gmail.com>
2019-10-03 17:03:47 +0530

    Add get-categories by org API : frontend

--- src/cljs/villagebookUI/api/category.cljs
+++ src/cljs/villagebookUI/api/category.cljs
@@ -1,7 +1,8 @@
 (ns villagebookUI.api.category
-  (:require [ajax.core :refer [POST]]))
+  (:require [ajax.core :refer [GET POST]]))
 
 (defn ^:private create-category-api [org-id] (str "/api/organisations/" org-id "/categories"))
+(defn ^:private get-categories-api [org-id] (str "/api/organisations/" org-id "/categories"))
 
 (defn create [category org-id handler error-handler]
   (POST (create-category-api org-id)
@@ -9,3 +10,9 @@
          :headers       {:content-type "application/json"}
          :handler       handler
          :error-handler error-handler}))
+
+(defn get-all [org-id handler error-handler]
+  (GET (get-categories-api org-id)
+       {:handler         handler
+        :response-format :json
+        :error-handler   error-handler}))
